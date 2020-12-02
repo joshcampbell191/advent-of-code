acc71aa4ddb8a359f4fca25e1fbbf42cc1656481
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-02 09:01:37 -0500

    Added Day 2 placeholder

---
+++ src/2020/02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/2020/02/README.md
@@ -0,0 +1,4 @@
+\--- Day 2: Password Philosophy ---
+-----------------------------
+
+See [Advent of Code](https://adventofcode.com/2020/day/2) for a full problem description.
