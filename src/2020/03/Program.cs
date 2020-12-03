f1e15bb0bfa158d86725f402db4e0aaefd766af7
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-03 15:28:41 -0500

    Added solution for day 3

--- src/2020/03/Program.cs
+++ src/2020/03/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.IO;
 
 namespace AdventOfCode
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("input.txt");
+
+            var partOne = PartOne(lines);
+            if (partOne > 0)
+                Console.WriteLine($"Part 1: {partOne}");
+            else
+                Console.WriteLine("Failed to solve Part 1");
+
+            var partTwo = PartTwo(lines);
+            if (partTwo > 0)
+                Console.WriteLine($"Part 2: {partTwo}");
+            else
+                Console.WriteLine("Failed to solve Part 2");
         }
+
+		private static long PartOne(string[] lines)
+		{
+			return TreesEncountered(lines, 3, 1);
+		}
+
+		private static long PartTwo(string[] lines)
+		{
+			var slopeOne = TreesEncountered(lines, 1, 1);
+			var slopeTwo = TreesEncountered(lines, 3, 1);
+			var slopeThree = TreesEncountered(lines, 5, 1);
+			var slopeFour = TreesEncountered(lines, 7, 1);
+			var slopeFive = TreesEncountered(lines, 1, 2);
+
+			return slopeOne * slopeTwo * slopeThree * slopeFour * slopeFive;
+		}
+
+		private static long TreesEncountered(string[] lines, int right, int down)
+		{
+			var treesEncountered = 0L;
+			var index = 0;
+
+			for (var i = 0; i < lines.Length; i += down)
+			{
+				var line = lines[i];
+				var length = line.Length;
+				index = index % length;
+
+				if (line[index] == '#')
+					treesEncountered++;
+
+				index += right;
+			}
+
+			return treesEncountered;
+		}
     }
 }
