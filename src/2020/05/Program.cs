801882dbe0f11d68b851deddc60991bd596226bf
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-05 20:14:11 -0500

    Added placeholder for day 5

---
+++ src/2020/05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			var lines = File.ReadAllLines("input.txt");
+			var numbers = lines.Select(s => int.Parse(s)).ToArray();
+
+			var partOne = PartOne(numbers);
+			if (partOne > 0)
+				Console.WriteLine($"Part 1: {partOne}");
+			else
+				Console.WriteLine("Failed to solve Part 1");
+
+			var partTwo = PartTwo(numbers);
+			if (partTwo > 0)
+				Console.WriteLine($"Part 2: {partTwo}");
+			else
+				Console.WriteLine("Failed to solve Part 2");
+		}
+
+		private static int PartOne(int[] numbers)
+		{
+            return 0;
+		}
+
+		private static int PartTwo(int[] numbers)
+		{
+			return 0;
+		}
+	}
+}
---
+++ src/2020/05/README.md
@@ -0,0 +1,4 @@
+\--- Day 5: Binary Boarding ---
+-----------------------------
+
+See [Advent of Code](https://adventofcode.com/2020/day/5) for a full problem description.
