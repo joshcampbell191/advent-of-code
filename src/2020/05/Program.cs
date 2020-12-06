cdc298210ca0c2c54f9c3f5171a9e6454c83af4e
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-05 23:49:24 -0500

    Added day 5 part 2

--- src/2020/05/Program.cs
+++ src/2020/05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -24,12 +25,55 @@ namespace AdventOfCode
 
 		private static int PartOne(string[] lines)
 		{
-            return 0;
+			return lines.Max(l => GetSeatId(l));
 		}
 
 		private static int PartTwo(string[] lines)
 		{
-			return 0;
+			var seatIds = lines.Select(l => GetSeatId(l)).OrderBy(s => s).ToArray();
+			if (seatIds.Length == 0)
+				return 0;
+				
+			var value = 0;
+			for(var i = 0; i < seatIds.Length - 1; i++)
+			{
+				value = seatIds[i];
+				if (seatIds[i + 1] != value + 1)
+					break;
+			}
+
+			return value + 1;
+		}
+
+		private static int GetSeatId(string boardingPass)
+		{
+			int front = 0, back = 127, left = 0, right = 7;
+
+			foreach (var c in boardingPass)
+			{
+				switch (c)
+				{
+					case 'F':
+						back -= (back - front) / 2 + 1;
+						break;
+					case 'B':
+						front += (back - front) / 2 + 1;
+						break;
+					case 'L':
+						right -= (right - left) / 2 + 1;
+						break;
+					case 'R':
+						left += (right - left) / 2 + 1;
+						break;
+				}
+			}
+
+			var row = front;
+			var column = left;
+
+			var seatId = row * 8 + column;
+
+			return seatId;
 		}
 	}
 }
