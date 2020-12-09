9fe51acfd30207363c0f78e688ac834bd001e8fe
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-09 12:09:21 -0500

    Added placeholder for day 9

---
+++ src/2020/09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			var lines = File.ReadAllLines("input.txt");
+
+			var partOne = PartOne(lines);
+			if (partOne > 0)
+				Console.WriteLine($"Part 1: {partOne}");
+			else
+				Console.WriteLine("Failed to solve Part 1");
+
+			var partTwo = PartTwo(lines);
+			if (partTwo > 0)
+				Console.WriteLine($"Part 2: {partTwo}");
+			else
+				Console.WriteLine("Failed to solve Part 2");
+		}
+
+		private static int PartOne(string[] lines)
+		{
+            return 0;
+		}
+
+		private static int PartTwo(string[] lines)
+		{
+			return 0;
+		}
+	}
+}
---
+++ src/2020/09/README.md
@@ -0,0 +1,4 @@
+\--- Day 9: Encoding Error ---
+-----------------------------
+
+See [Advent of Code](https://adventofcode.com/2020/day/9) for a full problem description.
