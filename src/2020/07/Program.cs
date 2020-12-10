7ee93f2fefb8846ad19f6b3bf7e074656cbd4843
Joshua Campbell <joshcampbell191@gmail.com>
2020-12-09 22:02:43 -0500

    Added solution for day 7 - part 2

--- src/2020/07/Program.cs
+++ src/2020/07/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
 {
 	public static class Program
 	{
-		public static Dictionary<string, List<string>> Bags;
+		public static Dictionary<string, Dictionary<string, int>> Bags;
 		public static void Main()
 		{
 			var lines = File.ReadAllLines("input.txt");
@@ -42,12 +42,11 @@ namespace AdventOfCode
 
 		private static int PartTwo()
 		{
-			return 0;
+			return CountBags(Bags["shiny gold"]);
 		}
 
-		private static Dictionary<string, List<string>> ParseBags(string[] lines)
+		private static Dictionary<string, Dictionary<string, int>> ParseBags(string[] lines)
 		{
-			var bags = new Dictionary<string, List<string>>();
 			var bagPattern = @"(?<color>.*) bags";
 			var contentPattern = @"(?<amount>\d+) (?<color>[\w\s]+) bags?[,\.]";
 
@@ -57,25 +56,34 @@ namespace AdventOfCode
 				.Select(x => new
 				{
 					Bag = Regex.Match(x[0], bagPattern).Groups["color"].Value,
-					Contents = Regex.Matches(x[1], contentPattern).Select(m => m.Groups["color"].Value).ToList()
+					Contents = Regex.Matches(x[1], contentPattern).ToDictionary(key => key.Groups["color"].Value, value => int.Parse(value.Groups["amount"].Value))
 				})
 				.ToDictionary(key => key.Bag, value => value.Contents);
 		}
 
-		private static bool ContainsBag(string content, List<string> bags)
+		private static bool ContainsBag(string content, Dictionary<string, int> bags)
 		{
 			// Does the bag contain content?
-			if (bags.Contains(content))
+			if (bags.ContainsKey(content))
 				return true;
 
 			// Does one of the other bags contain the content?
-			foreach (var bag in bags) {
+			foreach (var bag in bags.Keys) {
 				if (ContainsBag(content, Bags[bag]))
 					return true;
 			}
 
 			// The bag doesn't contain the content
 			return false;
+		}
+
+		private static int CountBags(Dictionary<string, int> bags)
+		{
+			var amount = 0;
+			foreach(var bag in bags) {
+				amount += bag.Value + bag.Value * CountBags(Bags[bag.Key]);
+			}
+			return amount;
 		}
 	}
 }
